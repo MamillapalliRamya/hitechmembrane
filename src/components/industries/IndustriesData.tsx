4cf6a6c22e9b01f962931a976bfccbd3963b81fb
Sindhu0906 <sriramineni.sindu@c360soft.ai>
2025-06-19 18:25:34 +0530

    implemented industries section and groupcompanies section in home page

--- src/components/industries/IndustriesData.tsx
+++ src/components/industries/IndustriesData.tsx
@@ -1,177 +1,177 @@
-import House from "../../assets/pics/image2.jpeg";
-import Health from "../../../src/assets/pics/image4.jpeg";
-import Replacement from "../../assets/images/wetransfer_hitech/Replacement1.jpg";
-import Municipal from "../../assets/images/wetransfer_hitech/Municipal.jpg";
-import Textile from "../../assets/images/wetransfer_hitech/Textile.jpeg";
-import FoodAndBeverage from "../../assets/images/wetransfer_hitech/Food_Beverage.jpg";
-import ProcessIndustries from "../../assets/images/wetransfer_hitech/Process_Industries.jpg";
-import CommercialInstitutions from "../../assets/images/wetransfer_hitech/Commercial_Institutions.jpeg";
+// import House from "../../assets/pics/image2.jpeg";
+// import Health from "../../../src/assets/pics/image4.jpeg";
+// import Replacement from "../../assets/images/wetransfer_hitech/Replacement1.jpg";
+// import Municipal from "../../assets/images/wetransfer_hitech/Municipal.jpg";
+// import Textile from "../../assets/images/wetransfer_hitech/Textile.jpeg";
+// import FoodAndBeverage from "../../assets/images/wetransfer_hitech/Food_Beverage.jpg";
+// import ProcessIndustries from "../../assets/images/wetransfer_hitech/Process_Industries.jpg";
+// import CommercialInstitutions from "../../assets/images/wetransfer_hitech/Commercial_Institutions.jpeg";
 
-export const IndustriesData = [
-  [
-    {
-      img: House,
-      title: "House",
-      headLine: "House",
-      content: [
-        {
-          list1:
-          "Hitech’s RO elements for drinking water perform consistently at low pressure for POU systems. Our bestsellers are 50 GPD, 75 GPD, and 100 GPD.",
-        },
-      ],
-    },
-  ],
-  [
-    {
-      img: Health,
-      title: "Health-Care",
-      headLine: "Health",
-      content: [
-        {
-          list1:
-            "Health-Care Industry requires the purest form of water for Dialysis Machines and research processes.our XLE series serves the requirements and also beneficial with low energy costs.",
-        },
-      ],
-    },
-  ],
-  [
-    {
-      img: Replacement,
-      title: "Replacement Markets",
-      headLine: "Replacement Markets",
-      content: [
-        {
-          list1:
-            "Our network allows replacing old RO elements with Hitech Membrane Elements, providing support and competitive pricing.",
-        },
-      ],
-    },
-  ],
+// export const IndustriesData = [
+//   [
+//     {
+//       img: House,
+//       title: "House",
+//       headLine: "House",
+//       content: [
+//         {
+//           list1:
+//           "Hitech’s RO elements for drinking water perform consistently at low pressure for POU systems. Our bestsellers are 50 GPD, 75 GPD, and 100 GPD.",
+//         },
+//       ],
+//     },
+//   ],
+//   [
+//     {
+//       img: Health,
+//       title: "Health-Care",
+//       headLine: "Health",
+//       content: [
+//         {
+//           list1:
+//             "Health-Care Industry requires the purest form of water for Dialysis Machines and research processes.our XLE series serves the requirements and also beneficial with low energy costs.",
+//         },
+//       ],
+//     },
+//   ],
+//   [
+//     {
+//       img: Replacement,
+//       title: "Replacement Markets",
+//       headLine: "Replacement Markets",
+//       content: [
+//         {
+//           list1:
+//             "Our network allows replacing old RO elements with Hitech Membrane Elements, providing support and competitive pricing.",
+//         },
+//       ],
+//     },
+//   ],
 
-  [
-    {
-      img: Municipal,
-      title: "Municipal",
-      headLine: "mnfctr-headline",
-      content: [
-        {
-          list1:
-            "Municipalities of all sizes accept our elements, appearing on approved brands lists for tenders.",
-          // list3: "mnfctr-desc-3",
-        },
-      ],
-    },
-  ],
+//   [
+//     {
+//       img: Municipal,
+//       title: "Municipal",
+//       headLine: "mnfctr-headline",
+//       content: [
+//         {
+//           list1:
+//             "Municipalities of all sizes accept our elements, appearing on approved brands lists for tenders.",
+//           // list3: "mnfctr-desc-3",
+//         },
+//       ],
+//     },
+//   ],
 
-  [
-    {
-      img: Textile,
-      title: "Textile",
-      headLine: "Textile",
-      content: [
-        {
-          list1:
-            "Textile and Dying houses need high RO elements for consistent performance at high temperatures. Our CP range of 4040 and 8040 RO elements meets these needs.",
-        },
-      ],
-    },
-  ],
+//   [
+//     {
+//       img: Textile,
+//       title: "Textile",
+//       headLine: "Textile",
+//       content: [
+//         {
+//           list1:
+//             "Textile and Dying houses need high RO elements for consistent performance at high temperatures. Our CP range of 4040 and 8040 RO elements meets these needs.",
+//         },
+//       ],
+//     },
+//   ],
 
-  [
-    {
-      img: FoodAndBeverage,
-      title: "Food and Beverage",
-      headLine: "tele-commun-headline",
-      content: [
-        {
-          list1:
-            "Be it Bottled water manufacturers or food and beverage processors we have specific solutions with our specialty elements.",
-          // list3: "tele-commun-desc-3",
-        },
-      ],
-    },
-  ],
-  [
-    {
-      img: ProcessIndustries,
-      title: "Process Industries",
-      headLine: "Process Industries",
-      content: [
-        {
-          list1:
-            "Our BW and ES range offers benefits like low fouling and high recovery. Our 4040 and 8040 RO elements are recognized for sustainable solutions.",
-          // list3: "auto-mob-desc-3",
-        },
-      ],
-    },
-  ],
-  [
-    {
-      img: CommercialInstitutions,
-      title: "Commercial Institutions",
-      headLine: "Commercial Institutions",
-      content: [
-        {
-          list1:
-            "Our RO elements use Hi-Tech's low energy ES series. Vending machines, offices, and hospitals utilize our 150, 300, and 400 GPD models for higher flux and low energy.",
-          // list3: "edu-desc-3",
-        },
-      ],
-    },
-  ],
-];
+//   [
+//     {
+//       img: FoodAndBeverage,
+//       title: "Food and Beverage",
+//       headLine: "tele-commun-headline",
+//       content: [
+//         {
+//           list1:
+//             "Be it Bottled water manufacturers or food and beverage processors we have specific solutions with our specialty elements.",
+//           // list3: "tele-commun-desc-3",
+//         },
+//       ],
+//     },
+//   ],
+//   [
+//     {
+//       img: ProcessIndustries,
+//       title: "Process Industries",
+//       headLine: "Process Industries",
+//       content: [
+//         {
+//           list1:
+//             "Our BW and ES range offers benefits like low fouling and high recovery. Our 4040 and 8040 RO elements are recognized for sustainable solutions.",
+//           // list3: "auto-mob-desc-3",
+//         },
+//       ],
+//     },
+//   ],
+//   [
+//     {
+//       img: CommercialInstitutions,
+//       title: "Commercial Institutions",
+//       headLine: "Commercial Institutions",
+//       content: [
+//         {
+//           list1:
+//             "Our RO elements use Hi-Tech's low energy ES series. Vending machines, offices, and hospitals utilize our 150, 300, and 400 GPD models for higher flux and low energy.",
+//           // list3: "edu-desc-3",
+//         },
+//       ],
+//     },
+//   ],
+// ];
 
-export const IndustriesIdData = [
-  {
-    id: "1",
-    name: "House",
-    color1: "#F4F6FC",
-    color: "#ffffff",
-  },
+// export const IndustriesIdData = [
+//   {
+//     id: "1",
+//     name: "House",
+//     color1: "#F4F6FC",
+//     color: "#ffffff",
+//   },
 
-  {
-    id: "2",
-    name: "Health",
-    color1: "#F4F6FC",
-    color: "#ffffff",
-  },
+//   {
+//     id: "2",
+//     name: "Health",
+//     color1: "#F4F6FC",
+//     color: "#ffffff",
+//   },
 
-  {
-    id: "3",
-    name: "Replacement Markets",
-    color1: "#F1F8E8",
-    color: "#ffffff",
-  },
+//   {
+//     id: "3",
+//     name: "Replacement Markets",
+//     color1: "#F1F8E8",
+//     color: "#ffffff",
+//   },
 
-  {
-    id: "4",
-    name: "Municipal",
-    color1: "#e4fff6",
-    color: "#ffffff",
-  },
+//   {
+//     id: "4",
+//     name: "Municipal",
+//     color1: "#e4fff6",
+//     color: "#ffffff",
+//   },
 
-  {
-    id: "5",
-    name: "Textile",
-    color1: "#e3ffd7",
-    color: "#ffffff",
-  },
-  {
-    id: "6",
-    name: "Food and Beverage",
-    color1: "#dfe8f9",
-    color: "#ffffff",
-  },
-  {
-    id: "7",
-    name: "Process Industries",
-    color1: "#D4FBF1",
-    color: "#ffffff",
-  },
-  {
-    id: "8",
-    name: "Commercial Institutions",
-    color1: "#F7E7DC",
-    color: "#ffffff",
-  },
-];
+//   {
+//     id: "5",
+//     name: "Textile",
+//     color1: "#e3ffd7",
+//     color: "#ffffff",
+//   },
+//   {
+//     id: "6",
+//     name: "Food and Beverage",
+//     color1: "#dfe8f9",
+//     color: "#ffffff",
+//   },
+//   {
+//     id: "7",
+//     name: "Process Industries",
+//     color1: "#D4FBF1",
+//     color: "#ffffff",
+//   },
+//   {
+//     id: "8",
+//     name: "Commercial Institutions",
+//     color1: "#F7E7DC",
+//     color: "#ffffff",
+//   },
+// ];
