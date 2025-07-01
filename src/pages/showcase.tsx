a29c9b8093de60d9a3edc26c2a589abfc81b6115
RamyaMamillapalli <ramya.mamillapalli@c360soft.ai>
2025-07-01 13:15:51 +0530

    added images

---
+++ src/pages/showcase.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import TitleSection from '../components/Titlesection';
+import GroupCompanies from '../components/home/GroupCompanies';
